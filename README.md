e5a86da6c33fb4a06a72fd4390bd0976d44384e9
Michael P. Soulier <msoulier@digitaltorque.ca>
2025-01-20 16:03:02 -0500

    Taking an argument on the command-line

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # jira
 A little tool to help me open links to a jira issue quickly from the command-line
+
+## Invocation
+jira <myproduct-1234>
+
+Right now this is hard-coding my data so it's not much help to anyone else. I will fix that soon.
--- main.go
+++ main.go
@@ -12,16 +12,25 @@ const (
 	JIRAURL_FMT string = "https://mitel.atlassian.net/browse/%s"
 )
 
+// This was written primary to run as "jira bug-1234" and it would
+// open the jira url for that bug for me.
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	writer := bufio.NewWriter(os.Stdout)
-	writer.WriteString("Jira issue number? ")
-	writer.Flush()
-	line, err := reader.ReadString('\n')
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "%s\n", err)
+    // If invoked with an issue number, we do not need to prompt for one.
+    issue := ""
+    if len(os.Args) > 1 {
+        issue = os.Args[1]
+    } else {
+        reader := bufio.NewReader(os.Stdin)
+        writer := bufio.NewWriter(os.Stdout)
+        writer.WriteString("Jira issue number? ")
+        writer.Flush()
+        line, err := reader.ReadString('\n')
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "%s\n", err)
+        }
+        issue = line
     }
-	issue := strings.TrimSpace(line)
+	issue = strings.TrimSpace(issue)
 	url := fmt.Sprintf(JIRAURL_FMT, issue)
 	fmt.Printf("Opening %s in your browser\n", url)
 	webbrowser.Open(url)
